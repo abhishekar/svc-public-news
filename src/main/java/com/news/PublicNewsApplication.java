97346831681d77727789afaef6031f29664d6c0f
abhishekar mechineni <abhishek0116@gmail.com>
2024-04-19 00:06:53 -0400

    added changes to feign client

--- src/main/java/com/news/PublicNewsApplication.java
+++ src/main/java/com/news/PublicNewsApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.news.client")
 public class PublicNewsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/news/client/NewsApiClient.java
+++ src/main/java/com/news/client/NewsApiClient.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @FeignClient(name = "newsApiClient", url = "${news.api.url}")
 public interface NewsApiClient {
 
